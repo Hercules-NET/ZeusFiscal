cc79a1fe6d01d0f29f93fb3d631c92d2a757e791
AO3\paulo.rodrigues <paulo.rodrigues@iob.com.br>
2025-11-10 09:08:25 -0300

    Alteração Campos RTC - NT 2025.002_v1.30 - NF-e/NFC-e

--- NFe.Classes/Informacoes/Detalhe/Tributacao/gIBSCredPres.cs
+++ NFe.Classes/Informacoes/Detalhe/Tributacao/gIBSCredPres.cs
@@ -8,28 +8,24 @@ namespace NFe.Classes.Informacoes.Detalhe.Tributacao
         private decimal _vCredPres;
         private decimal _vCredPresCondSus;
 
-        // UB74
+        // UB124
         [XmlElement(Order = 1)]
-        public TipocCredPres cCredPres { get; set; }
-
-        // UB75
-        [XmlElement(Order = 2)]
         public decimal pCredPres
         {
             get => _pCredPres.Arredondar(4);
             set => _pCredPres = value.Arredondar(4);
         }
 
-        // UB76
-        [XmlElement(Order = 3)]
+        // UB125
+        [XmlElement(Order = 2)]
         public decimal vCredPres
         {
             get => _vCredPres.Arredondar(2);
             set => _vCredPres = value.Arredondar(2);
         }
 
-        // UB77
-        [XmlElement(Order = 4)]
+        // UB126
+        [XmlElement(Order = 3)]
         public decimal vCredPresCondSus
         {
             get => _vCredPresCondSus.Arredondar(2);
