119592e7f926608f3edea3724d68e0f648691eee
Thiago Batista <rodrigo@visualmanager.com.br>
2026-03-31 11:39:03 -0300

    adicionando Evento Cancelamento Desacordo em CTe.Servicos

---
+++ CTe.Servicos/Eventos/EventoCancelamentoDesacordo.cs
@@ -0,0 +1,47 @@
+﻿using CTe.Classes;
+using CTe.Classes.Servicos.Evento;
+using CTe.Classes.Servicos.Evento.Flags;
+using CTe.Servicos.Factory;
+using System.Threading.Tasks;
+
+namespace CTe.Servicos.Eventos
+{
+    public class EventoCancelamentoDesacordo
+    {
+        private readonly int _sequenciaEvento;
+        private readonly string _cnpj;
+        private readonly string _chave;
+        private readonly string _nProtEvPrestDes;
+
+        public eventoCTe EventoEnviado { get; private set; }
+        public retEventoCTe RetornoSefaz { get; private set; }
+
+        public EventoCancelamentoDesacordo(int sequenciaEvento, string chave, string cnpj, string nProtEvPrestDes)
+        {
+            _chave = chave;
+            _cnpj = cnpj;
+            _sequenciaEvento = sequenciaEvento;
+            _nProtEvPrestDes = nProtEvPrestDes;
+        }
+
+        public retEventoCTe CancelarDesacordo(ConfiguracaoServico configuracaoServico = null)
+        {
+            var eventoCancelaDiscordar = ClassesFactory.CriaEvCancPrestDesacordo(_nProtEvPrestDes);
+
+            EventoEnviado = FactoryEvento.CriaEvento(CTeTipoEvento.CancelamentoPrestacaodoServicoemDesacordo, _sequenciaEvento, _chave, _cnpj, eventoCancelaDiscordar, configuracaoServico);
+            RetornoSefaz = new ServicoController().Executar(CTeTipoEvento.CancelamentoPrestacaodoServicoemDesacordo, _sequenciaEvento, _chave, _cnpj, eventoCancelaDiscordar, configuracaoServico);
+
+            return RetornoSefaz;
+        }
+
+        public async Task<retEventoCTe> CancelarDesacordoAsync(ConfiguracaoServico configuracaoServico = null)
+        {
+            var eventoCancelaDiscordar = ClassesFactory.CriaEvCancPrestDesacordo(_nProtEvPrestDes);
+
+            EventoEnviado = FactoryEvento.CriaEvento(CTeTipoEvento.CancelamentoPrestacaodoServicoemDesacordo, _sequenciaEvento, _chave, _cnpj, eventoCancelaDiscordar, configuracaoServico);
+            RetornoSefaz = await new ServicoController().ExecutarAsync(CTeTipoEvento.CancelamentoPrestacaodoServicoemDesacordo, _sequenciaEvento, _chave, _cnpj, eventoCancelaDiscordar, configuracaoServico);
+
+            return RetornoSefaz;
+        }
+    }
+}
